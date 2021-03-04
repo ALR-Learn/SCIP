ec6b7c207d79897772aaf362f4b931bcc5e8aa62
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2021-03-04 12:03:26 -0600

    week one hw

--- Homework/week-1/homework1.rkt
+++ Homework/week-1/homework1.rkt
@@ -1,7 +1,9 @@
 ;; Uncomment if you want to run scheme examples/answers
-;#lang simply-scheme
-#lang racket
-(require test-engine/racket-tests)
+;; and comment racket answers 
+#lang simply-scheme
+;; Comment both lines if you want to run scheme answers.
+;#lang racket
+;(require test-engine/racket-tests)
 
 ;; Question 2
 ;; Write a procedure squares that takes a sentence of number as its argument
@@ -23,12 +25,20 @@
 ;; Racket Answer
 ;; Number -> Number
 ;; Purpose is to produce a number multipied by itself
-(check-expect (square 10) 10)
+;(check-expect (square 10) 100)
+;
+;(define (square x)
+;  (* x x))
 
-(define (square x)
-  (* x x))
+;; List<Number> -> List<Number>
+;; Purpose is to produce a list of numbers that are the squares of the arguments
+;(check-expect (squares '(1 2 3 4)) '(1 4 9 16))
+;
+;(define (squares x)
+;  (if (empty? x)
+;      (list)
+;      (cons (square (first x)) (squares (rest x)))))
 
-(test)
 
 ;; Question 3
 ;; Write a procedure switch that takes a sentence as its argument and returns a
@@ -55,3 +65,39 @@
 
 ;(switch '(I told))
 ;(switch '(you told me that I should wake you up))
+
+
+;; Question 4
+;; Write a predicate ordered? that takes a sentece of numbers as its argument
+;; and returns a true value if the numbes are ascending order or false value otherwise
+
+;(define (ordered? numbers)
+;  (if (< (count numbers) 2)
+;      'false
+;      (if (and (< (first numbers) (item 2 numbers)) (ordered? (bf numbers)))
+;          'true
+;          'false)))
+
+;(ordered? '(2 1))
+;(ordered? '(1 2))
+;(ordered? '(1 2 3 4))
+;(ordered? '(4 3 2 1))
+
+;; Question 5
+;; Write a procedure ends-e that takes a sentence as its argument and returns a sentence
+;; containing only those words of the argument whose last letter is E
+
+;(define (ends-e str)
+;  (if (empty? str)
+;      (se)
+;      (if (equal? (last (first str)) 'e)
+;          (se (first str) (ends-e (bf str)))
+;          (se (ends-e(bf str))))))
+;
+;(ends-e '(please put the salami above the blue elephant))
+;; Expected
+;; '(please the above the blue)
+
+
+;(test)
+
